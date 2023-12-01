0eef8e3b386259bf2f6dac88b387bb76a20a7463
nafisreza <bigganinafis@gmail.com>
2023-12-01 09:14:14 +0600

    🎨 Quiz app UI designed

---
+++ quiz-app/script.js
@@ -0,0 +1,136 @@
+const questions = [
+    {
+        "question": "What temperature does water boil at?",
+        "A": "50 degrees Celcius",
+        "B": "25 degrees Celcius",
+        "C": "100 degrees Celcius",
+        "D": "150 degrees Celcius",
+        "answer": "C"
+    },
+
+    {
+        "question": "Who wrote Julius Caesar, Macbeth and Hamlet?",
+        "A": "Wole Soyinka",
+        "B": "William Shakespeare",
+        "C": "Ngozi Chimamanda Adichie",
+        "D": "Dan Brown",
+        "answer": "B"
+    },
+
+    {
+        "question": "What did the crocodile swallow in Peter Pan?",
+        "A": "A Book",
+        "B": "A Computer",
+        "C": "A pair of shoes",
+        "D": "Alarm Clock",
+        "answer": "D"
+    },
+
+    {
+        "question": "Which is the only mammal that can’t jump?",
+        "A": "Dog",
+        "B": "Elephant",
+        "C": "Goat",
+        "D": "Lion",
+        "answer": "B"
+    },
+
+    {
+        "question": "Who lived at 221B, Baker Street, London?",
+        "A": "Tony Stark",
+        "B": "Morgan Freeman",
+        "C": "Sherlock Holmes",
+        "D": "Samuel L. Jackson",
+        "answer": "C"
+    },
+
+    {
+        "question": "What colour is a panda?",
+        "A": "Green and Yellow",
+        "B": "Blue and Red",
+        "C": "Green and White",
+        "D": "Black and White",
+        "answer": "D"
+    },
+
+    {
+        "question": "Where is the smallest bone in the human body?",
+        "A": "The Chest",
+        "B": "The Ear",
+        "C": "The Legs",
+        "D": "The Hands",
+        "answer": "B"
+    },
+
+    {
+        "question": "What does the roman numeral C represent?",
+        "A": "100",
+        "B": "10",
+        "C": "10,000",
+        "D": "1,000,000",
+        "answer": "A"
+    },
+
+    {
+        "question": "What takes place in Hong Kong's Happy Valley?",
+        "A": "Chicken Wrestling",
+        "B": "Horse racing",
+        "C": "Street Racing",
+        "D": "Arm Wrestling",
+        "answer": "B"
+    },
+
+    {
+        "question": "Who painted the Mona Lisa?",
+        "A": "Alexander Graham Bell",
+        "B": "Sir Isaac Newton",
+        "C": "Leonardo Da Vinci",
+        "D": "Albert Einstein",
+        "answer": "C"
+    },
+
+    {
+        "question": "What’s the most important book in the Moslem religion?",
+        "A": "The Quran",
+        "B": "The Dictionary",
+        "C": "The Bible",
+        "D": "The Chemistry text Book",
+        "answer": "A"
+    },
+
+    {
+        "question": "What’s the capital of Bangladesh?",
+        "A": "Chittagong",
+        "B": "Sylhet",
+        "C": "Dhaka",
+        "D": "Rajshahi",
+        "answer": "C"
+    },
+
+    {
+        "question": "How many squares are there on a chess board?",
+        "A": "128",
+        "B": "64",
+        "C": "32",
+        "D": "256",
+        "answer": "B"
+    },
+
+    {
+        "question": "Who invented the electric light bulb?",
+        "A": "Tom Cruise",
+        "B": "Barack Obama",
+        "C": "Wole Soyinka",
+        "D": "Thomas Edison",
+        "answer": "D"
+    },
+
+    {
+        "question": "What are the first three words of the bible?",
+        "A": "be with everyone",
+        "B": "Again Jesus asked",
+        "C": "In the beginning",
+        "D": "At that time",
+        "answer": "C"
+    }
+]
